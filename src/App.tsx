32eaba8da0bfb0f2ef81dcbc0cf54215f8eb2865
francosalama <53917270+francosalama@users.noreply.github.com>
2021-11-05 11:51:13 -0300

    conectarse a metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import contractMap from '@rsksmart/rsk-testnet-contract-metadata'
 import Row from './components/row'
+import RLogin from '@rsksmart/rlogin'
 
 const baseDir =
   'https://raw.githubusercontent.com/rsksmart/rsk-testnet-contract-metadata/master/images/'
 const addresses = Object.keys(contractMap)
+const rpcUrls = {
+  31: 'https://public-node.testnet.rsk.co'
+}
+const supportedChains = Object.keys(rpcUrls).map(Number)
+
+const rLogin = new RLogin({
+  rpcUrls,
+  supportedChains
+})
 
 function App () {
+  const [mm, setMM] = useState(null)
+  const handleLogin = () => {
+    rLogin.connect().then(({ provider }) => setMM(provider))
+  }
   return (
     <div className="App">
       RSK Token Faucet
       {addresses.map((address: any) => <Row key={address} token = {contractMap[address]} add = {address} />)}
+      <button onClick = {handleLogin} >Log In</button>
     </div>
+
   )
 }
 

4372ffed86223baf533260c7c3a04a3424b7114f
francosalama <53917270+francosalama@users.noreply.github.com>
2021-11-26 09:41:01 -0300

    Portis

--- src/rLogin.ts
+++ src/rLogin.ts
@@ -1,5 +1,6 @@
 import RLogin from '@rsksmart/rlogin'
 import WalletConnectProvider from '@walletconnect/web3-provider'
+import Portis from '@portis/web3'
 
 const rpcUrls = {
   31: 'https://public-node.testnet.rsk.co'
@@ -10,12 +11,23 @@ const supportedChains = Object.keys(rpcUrls).map(Number)
 const rLogin = new RLogin({
   rpcUrls,
   supportedChains,
+  cacheProvider: false,
   providerOptions: {
     walletconnect: {
       package: WalletConnectProvider,
       options: {
         rpc: rpcUrls,
         bridge: 'https://walletconnect-bridge.rifos.org/'
+      }
+    },
+    portis: {
+      package: Portis,
+      options: {
+        id: 'a1c8672b-7b1c-476b-b3d0-41c27d575920',
+        network: {
+          nodeUrl: 'https://public-node.testnet.rsk.co',
+          chainId: 31
+        }
       }
     }
   }
